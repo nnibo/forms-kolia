13b2879de1e0f69b0113dbb60c2b28f3f38da101
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-04 10:57:37 -0300

    tailwind fucionando

--- forms-kolia/src/Componentes/Header.jsx
+++ forms-kolia/src/Componentes/Header.jsx
@@ -1,8 +1,14 @@
 export default function Header(){
     return(
         <>
-         <div className="bg-red-500 w-32 h-32"></div>
-         <p>Piroca</p>
+         <div className="bg-zinc-900 w-full">
+            <img className="w-96 p-5" src="../../public/logoKolia.PNG" alt="" />
+         </div>
+
+         <h1 className="text-center font-bold">Realize o Pré Check-In</h1>
+
+         <form action=""></form>
+         
         </>
     )
 }
