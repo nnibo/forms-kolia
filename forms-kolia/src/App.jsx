ad92cb9e58a907d2729bdce335b3a301c55d0f77
nnibo <nbaradel4@gmail.com>
2024-10-03 23:19:04 -0300

    erro no tailwind

---
+++ forms-kolia/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Componentes/Header"
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
+
---
+++ forms-kolia/src/Componentes/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <>
+         <div className="bg-red-500 w-32 h-32"></div>
+         <p>Piroca</p>
+        </>
+    )
+}
